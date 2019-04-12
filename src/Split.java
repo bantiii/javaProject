414fb227c433df45ef38e70def46c1b6df0780a1
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-12 13:06:06 -0700

    this is another one

--- src/Split.java
+++ src/Split.java
@@ -9,6 +9,8 @@ public class Split {
 	//in the very good fashion of life so don't worry we gonna make it.
 	String[] nameSplit =name.split("\\s");
 	for(int i=0; i<nameSplit.length;i++){
+		
+		//this is new bro
 		System.out.println("the value of the name is then changed"+nameSplit[i]);
 	}
 	}
