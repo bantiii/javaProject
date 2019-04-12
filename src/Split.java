0ed5d94ac8e034da12efb6e67e88c23e9993d18a
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-12 13:01:51 -0700

    just added some information in the project

--- src/Split.java
+++ src/Split.java
@@ -4,6 +4,9 @@ public class Split {
 		Date objdat = new Date();
 	System.out.println("======"+objdat+"=============");
 	String name ="banataelak tahefet ayalew";
+	// this is gonna be the First order list value of the product
+	// and also this will make our jorny perfect and we will try to cover come all this 
+	//in the very good fashion of life so don't worry we gonna make it.
 	String[] nameSplit =name.split("\\s");
 	for(int i=0; i<nameSplit.length;i++){
 		System.out.println("the value of the name is then changed"+nameSplit[i]);
